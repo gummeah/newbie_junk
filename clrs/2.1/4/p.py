515e4a8b8cb1edefdeb13c27fc519b645008f36a
gummeah <gummeah@gmail.com>
2017-09-26 22:50:48 +0300

    removed some test stuff

--- clrs/2.1/4/p.py
+++ clrs/2.1/4/p.py
@@ -1,21 +1,15 @@
 #!python3
-import time
-a = input()
-b = input()
-print(a, b)
-a = [int(i) for i in a]
-b = [int(i) for i in b]
-n = max(len(a), len(b))
-c = [0] * (n + 1)
-start_time = time.time()
-for i in range(n-1, -1, -1):
-    c[i+1] = c[i+1] + a[i] + b[i]
-    if c[i+1] == 3:
-        c[i+1] = 1 
-        c[i] = 1
-    elif c[i+1] == 2:
-        c[i+1] = 0
-        c[i] = 1
 
-print(''.join(map(str, c)))
-print("--- %s seconds ---" % (time.time() - start_time))
+def bin_add(a, b):
+    n = max(len(a), len(b))
+    c = [0] * (n + 1)
+    for i in range(n-1, -1, -1):
+        c[i+1] = c[i+1] + a[i] + b[i]
+        if c[i+1] == 3:
+            c[i+1] = 1 
+            c[i] = 1
+        elif c[i+1] == 2:
+            c[i+1] = 0
+           c[i] = 1
+
+    return c
--- clrs/2.1/4/p1.py
+++ clrs/2.1/4/p1.py
@@ -1,18 +1,13 @@
 #!python3
-import time
-a = input()
-b = input()
-print(a, b)
-a = [int(i) for i in a]
-b = [int(i) for i in b]
-n = max(len(a), len(b))
-c = [0] * (n + 1)
-start_time = time.time()
-carry = 0
-for i in range(n-1, -1, -1):
-    c[i+1] = (a[i] + b[i] + carry) % 2
-    carry = (a[i] + b[i] + carry) // 2
-c[0] = carry
 
-print(''.join(map(str, c)))
-print("--- %s seconds ---" % (time.time() - start_time))
+def bin_add(a, b):
+    n = max(len(a), len(b))
+    c = [0] * (n + 1)
+    carry = 0
+    for i in range(n-1, -1, -1):
+        c[i+1] = (a[i] + b[i] + carry) % 2
+        carry = (a[i] + b[i] + carry) // 2
+    c[0] = carry
+    
+    return c
+
